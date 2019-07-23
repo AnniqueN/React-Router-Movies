fbf13bf4ab87d8035c93afbfbfa6c8773b767406
Annique Nsabimana <anniquensabimana@gmail.com>
2019-07-22 19:50:24 -0500

    added savefunction added browser router Homefunction

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,31 @@
-import React, { useState } from 'react';
-import { Route } from "react-router-dom";
-import SavedList from './Movies/SavedList';
+import React, { useState } from "react";
+import { Switch, Route } from "react-router-dom";
+
+import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+  const [savedList, setSavedList] = useState([]);
 
-  function addToSavedList(movie) {
-    setSavedList( [...savedList, movie] );
+  const addToSavedList = movie => {
+    setSavedList([...savedList, movie]);
   };
 
   return (
     <div>
-      <Route to="/" component={MovieList} />
-      <Route to={`/movies/:id`} component={MovieList} />
       <SavedList list={savedList} />
+
+      <Switch>
+        <Route exact path="/" component={MovieList} />
+        <Route
+          exact
+          path="/movies/:id"
+          component={({ match }) => (
+            <Movie match={match} addToSavedList={addToSavedList} />
+          )}
+        />
+      </Switch>
     </div>
   );
 };
